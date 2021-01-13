119861402c7ed856d347b3caf6685162ac9f19fa
Adam Bahrani <adbarani2@gmail.com>
2021-01-13 16:13:28 -0500

    Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import React, { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./ehr_components/home";
 import Search from "./ehr_components/search";
-// import Home from "./ehr_components/search"
-
-// import { Bootstrap } from "bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "jquery/dist/jquery.min.js";
-// import "bootstrap/dist/js/bootstrap.min.js";
 
 import "./App.css";
 

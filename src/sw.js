5354ce86c07b8149e9ae149b457e031fbaef49d5
Adam Bahrani <adbarani2@gmail.com>
2021-01-18 10:33:55 -0500

    Activate offline feature

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,8 @@
 export default function swDev() {
+  console.log("SwDev");
   let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
-  navigator.serviceWorker.register(swUrl).then(res =>{console.log(res)});
+  navigator.serviceWorker.register(swUrl).then(res => {
+    console.log(res);
+  });
 }

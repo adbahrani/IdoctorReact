156410b10d36d94ba339e24703ebe8247140cc40
DESKTOP-3P24579\Adam <adbarani2@gmail.com>
2021-11-30 16:53:33 -0500

    Styles Updates

--- client/src/ehr_components/SearchTable/styles.tsx
+++ client/src/ehr_components/SearchTable/styles.tsx
@@ -1,8 +1,8 @@
 export const columnWidths: { [char: string]: string } = {
-  fullName: "col-4 col-md-3",
-  dob: "col-4 col-md-2",
-  phoneNumber: "col-4 col-md-3",
-  actions: "col-12 col-md-4"
+  fullName: "col-4 col-md-3 col-lg-3",
+  dob: "col-4 col-md-2 col-lg-2",
+  phoneNumber: "col-4 col-md-3 col-lg-2",
+  actions: "col-12 col-md-4 col-lg-5"
 };
 
 export const columnStyles: { [char: string]: string } = {
